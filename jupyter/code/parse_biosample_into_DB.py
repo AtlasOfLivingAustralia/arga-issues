1f49547339f19f0189df7e31a7d442f1bbf9466a
Nick dos Remedios <nick.dosremedios@csiro.au>
2022-06-06 17:06:08 +1000

    WIP

--- jupyter/code/parse_biosample_into_DB.py
+++ jupyter/code/parse_biosample_into_DB.py
@@ -1,44 +1,34 @@
 from bigxml import Parser, xml_handle_element, xml_handle_text
 from dataclasses import dataclass, field
-from typing import Dict, List
 from datetime import datetime
-import sqlite3 as sl
+from datalite import datalite
+from datalite.fetch import fetch_if, fetch_all, fetch_range, fetch_from, fetch_equals, fetch_where
+from datalite.migrations import basic_migrate, _drop_table
 import json
+import progressBar as pgb
 
 start = datetime.now()
 print("Start:",start)
-
-con = sl.connect('../data/biosamples_medium.db')
-
-with con:
-    con.execute("""
-        DROP TABLE IF EXISTS SAMPLE;
-    """)
-
-with con:
-    con.execute("""
-        CREATE TABLE IF NOT EXISTS SAMPLE (
-            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-            accession TEXT,
-            email TEXT,
-            link_url TEXT,
-            attribute_json TEXT
-        );
-    """)
-
-with con:
-    con.execute("""
-        CREATE INDEX idx_sample_accession on SAMPLE(accession);
-""") 
-
-sql = 'INSERT INTO SAMPLE (accession, email, link_url, attribute_json) values(?, ?, ?, ?)'
+# clear DB
+#_drop_table('../data/biosamples_full.db', 'Entry')
+# CREATE INDEX idx_entry_id on ENTRY(id);
 
 # See example record HTML version: https://www.ncbi.nlm.nih.gov/biosample/?term=SAMN06198159
 @xml_handle_element("BioSampleSet", "BioSample")
+@datalite(db_path="../data/biosamples_DBL.db")
 @dataclass
 class Entry:
-    id: str 
+    """
+    A single record from the BioSample Set XML file from NCBI
+
+    A subset of fields and values are pulled out and stored in a SQLite3 database. 
+    This DB will be used in the DwC file code to populate additional values not present
+    in the NCBI-refseq CSV dump from NCBI.
+    """
+    id: str = ''
+    scientificName: str = ''
     email: str = ''
+    lab: str = ''
     url: str = ''
     dwc_EarliestDateCollected: str = ''
     dwc_otherCatalogNumbers: str = ''
@@ -46,31 +36,45 @@ class Entry:
     dwc_lifeStage: str = ''
     dwc_MaterialSample: str = ''
     dwc_materialSampleID: str  = ''
-    attrs: Dict[str, str] = field(default_factory=dict)
+    attrs: str = ''
 
     def __init__(self, node):
+        # <BioSample submission_date="2008-07-24T12:44:10.763" last_update="2013-06-12T08:42:39.317" publication_date="2008-09-15T09:31:24.973" access="public" id="355" accession="SAMN00000355">
         if node.attributes.get('accession'):
             self.id = node.attributes['accession']
         elif node.attributes.get('id'):
             self.id = "SAMN" + node.attributes['id'].zfill(8)
         else:
             self.id = 'none'
-        self.attrs = {}
+        #self.attrs = {}
 
+    # <Organism taxonomy_id="4932" taxonomy_name="Saccharomyces cerevisiae"/>
+    @xml_handle_element("Description","Organism")
+    def handle_organism(self, node):
+        if node.attributes.get('taxonomy_name'):
+            self.scientificName = node.attributes['taxonomy_name']
+
+    # <Contact email="shuse@mbl.edu" lab="MARBILAB">
     @xml_handle_element("Owner","Contacts","Contact")
     def handle_email(self, node):
         if node.attributes.get('email'):
             self.email = node.attributes['email']
+        if node.attributes.get('lab'):
+            self.lab = node.attributes['lab']
 
+    # <Attribute attribute_name="strain" harmonized_name="strain" display_name="strain">BY4741</Attribute>
     @xml_handle_element("Attributes","Attribute")
     def handle_attribute(self, items):
         # Attributes can have different field names - pick the "best" available
         thisAttr = {} # use a dict to strore attribute fields
+        if self.attrs:
+            thisAttr = json.loads(self.attrs) # load any existing data from previous Attribute entries     
         if items.attributes.get('harmonized_name'):
-            thisAttr = { items.attributes['harmonized_name']: items.text }
+            thisAttr.update({ items.attributes['harmonized_name']: items.text })
         elif items.attributes.get('attribute_name'):
-            thisAttr = { items.attributes['attribute_name']: items.text }
-        self.attrs.update(thisAttr) # add to dataclass field (dict)
+            thisAttr.update({ items.attributes['attribute_name']: items.text })
+        #self.attrs.update(thisAttr) # add to dataclass field (dict)
+        self.attrs = json.dumps(thisAttr) # JSON stringified
         # extract any DwC fields so upstream code doesn't need to know about the XML structure
         if (items.attributes.get('attribute_name') and items.attributes['attribute_name'] == 'collection_date'):
             self.dwc_EarliestDateCollected = items.text
@@ -85,27 +89,32 @@ class Entry:
         if (items.attributes.get('attribute_name') and items.attributes['attribute_name'] == 'isolate'):
             self.dwc_materialSampleID = items.text
 
+    # <Link type="url" label="GEO Web Link">http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSM282598</Link>
     @xml_handle_element("Links","Link")
     def handle_link(self, link):
         if link.attributes['type'] == 'url':
             self.url = link.text
 
-
-with open("/data/arga-data/biosample_set_big.xml", "rb") as f:
+debug = False
+totalRecords = 26497644
+count = 0
+with open("/data/arga-data/biosample_set.xml", "rb") as f:
     for item in Parser(f).iter_from(Entry):
-        print("> ",item)
-        jsonStr = json.dumps(item.attrs)
-        sqlData = [(item.id, item.email, item.url, jsonStr)]
-        with con:
-            con.executemany(sql, sqlData)
+        count +=  1
+        if (debug and count % 1000 == 0):
+            print(count,item)
+        # if (not debug and count % 10000 == 0):
+        #     print('.', end='', sep='')
+        # if count % 800000 == 0:
+        #     print('') # 80 column line length
+        pgb.printProgress(count, totalRecords)
+        item.create_entry()  # Adds the entry to the table associated in db.db.
 
-with con:
-    data = con.execute("SELECT * FROM SAMPLE LIMIT 10")
-    for row in data:
-        print(row)
-    count = con.execute("SELECT COUNT(*) FROM SAMPLE")
-    print("count =", count.fetchone()[0])
+# print('') # force a newline
+# Show first 10 entries - failing for some reason
+# print(fetch_all(Entry, 1, 10))
 
 end = datetime.now()
 print("End:", end)
+print("Count =","{:,d}".format(count))
 print("The total execution time in seconds is:", str(end-start)[:9])
---
+++ jupyter/code/parse_biosample_lxml.py
@@ -0,0 +1,41 @@
+import sys
+import xml.etree.ElementTree as ET
+from lxml import etree
+import time
+
+file_path = "/data/arga-data/biosample_set_small.xml"
+count = 0
+
+tic = time.perf_counter()
+for event, elem in ET.iterparse(file_path, events=("end",)):
+    if event == "end":
+        if elem.tag == 'Attribute':
+            count += 1
+        elem.clear()
+toc = time.perf_counter()
+print(f"1. Total parse time = {toc - tic:0.4f} seconds")
+print("Count of Attribute tags:", count)
+
+count1 = 0
+tic1 = time.perf_counter()
+#for event, element in etree.iterparse(file_path, tag="Attribute"):
+for ev, element in etree.iterparse(file_path):
+    # if element.get('accession'):
+    #     print('accession:', element.get('accession'))
+    #for child in element:
+        #print(child.tag, child.text, child.keys(), sep='|')
+    #nameEl = etree.SubElement(element, "Name")
+    # nameEl = element.xpath('//Name')
+    # for el in nameEl:
+    #     print("Name:", el.text, "| abbreviation:", el.get('abbreviation'))
+    # attributeEl = element.xpath('//Attribute')
+    # for el in attributeEl:
+    #     count1 += 1
+    #if ev == "end":
+        if element.tag == 'Attribute':
+            count1 += 1
+        element.clear()
+    
+toc1 = time.perf_counter()
+print(f"2. Total parse time = {toc1 - tic1:0.4f} seconds")
+print("Count of Attribute tags:", count1)
