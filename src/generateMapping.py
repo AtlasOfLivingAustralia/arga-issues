890f9bd4c84caff25e6aa1e71025c407a3acceb7
c-mang <christopher.mangion@hotmail.com>
2022-08-18 15:24:40 +1000

    - Added reqs.txt for setting up virtual env - Added mapping generation for dwc extensions - Mapping generation also creates blank json map - Added some mapping for ENA - Added helperFunctions.py for generic helper functions

---
+++ src/generateMapping.py
@@ -0,0 +1,45 @@
+import requests
+import json
+import xmltodict
+import os
+
+# Generate mapping json from all dwc extensions
+# Additionally, generate a helpful collation of all dwc extensions available
+
+dwcExtensions = "https://rs.gbif.org/extensions.json"
+outputFolder = "../generatedFiles"
+
+response = requests.get(dwcExtensions)
+extensionMap = response.json()
+
+rawOutput = {}
+dwcCollation = {}
+conversionMap = {}
+
+for ext in extensionMap["extensions"]:
+    subcatagory = requests.get(ext["url"])
+    decodedSubcatagory = subcatagory.content.decode("utf-8")
+    output = json.loads(json.dumps(xmltodict.parse(decodedSubcatagory)))
+    rawOutput[ext["title"]] = output
+
+    for prop in output["extension"]["property"]:
+        dwcCollation[prop["@name"]] = {
+                "group": prop.get("@group", ""),
+                "relation": prop.get("@dc:relation", ""),
+                "description": prop.get("@dc:description", "No description available."),
+                "examples": prop.get("@examples", ""),
+                "qualName": prop.get("@qualName", ""),
+                "required": prop.get("@required", "Unknown")
+            }
+
+        conversionMap[prop["@name"]] = []
+
+with open(os.path.join(outputFolder, "rawDwCExtensions.json"), "w") as fp:
+    json.dump(rawOutput, fp, indent=4)
+
+with open(os.path.join(outputFolder, "DwCExtensionCollation.json"), "w") as fp:
+    json.dump(dwcCollation, fp, indent=4)
+
+if not os.path.exists("../mapping.json"):
+    with open("../mapping.json", "w") as fp:
+        json.dump(conversionMap, fp, indent=4)
