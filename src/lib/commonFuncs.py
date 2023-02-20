d1718b6fef3183bbd516707a8d55901e710e4e94
Christopher Mangion <christopher.mangion@hotmail.com>
2023-02-20 14:25:18 +1000

    Added a lib folder to allow importing anywhere within project Moved config to lib folder Replaced helperFunctions.py with commonFuncs.py Added new dataframeFuncs.py for dataframe specific functions

---
+++ src/lib/commonFuncs.py
@@ -0,0 +1,134 @@
+import logging
+import csv
+import lib.config as cfg
+import json
+import requests
+import re
+from requests.auth import HTTPBasicAuth
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+
+def reverseLookup(lookupDict):
+    return {oldName: newName for newName, oldNameList in lookupDict.items() for oldName in oldNameList}
+    
+def latlongToDecimal(latlong):
+    """
+    Paramters:
+        latlong: string representation of both latitude and longitude
+
+    Converts the latitude and longitude string to separate lat and long strings, returned as a tuple.
+    """
+    latlong = str(latlong)
+    split = latlong.split(' ')
+    if len(split) == 4:
+        latlong = f"{split[1]}{split[0]} {split[3]}{split[2]}"
+    elif len(split) == 2:
+        latlong = f"{split[0][-1]}{split[0][:-1]} {split[1][-1]}{split[1][:-1]}"
+    else:
+        return latlong
+
+    latlong = latlong.replace('S', '-').replace('W', '-').replace('N', '').replace('E', '')
+    return latlong.split(' ')
+
+def loadLogger(filename):
+    logging.basicConfig(
+        filename=cfg.folderPaths.logs / f"{filename}.log",
+        encoding="utf-8",
+        level=logging.DEBUG,
+        filemode='w',
+        format="%(message)s"
+    )
+
+    return logging.getLogger(__name__)
+
+def flatten(d):
+    res = {}
+    
+    for key, value in d.items():
+        if isinstance(value, dict):
+            res.update(flatten(value))
+        elif isinstance(value, list) or isinstance(value, tuple):
+            for item in value:
+                if isinstance(item, dict):
+                    res.update(flatten(item))
+                else:
+                    res[key] = item
+        else:
+            res[key] = value
+
+    return res
+
+def getColumns(filepath, separator=',', headerRow=0):
+    with open(filepath, encoding='utf-8') as fp:
+        reader = csv.reader(fp, delimiter=separator)
+        for idx, row in enumerate(reader):
+            if idx == headerRow:
+                return row
+
+def extendUnique(lst, values):
+    lst.extend([item for item in values if item not in lst])
+    return lst
+
+def addUniqueEntry(dictionary, key, value, duplicateLimit=0):
+    if key not in dictionary:
+        dictionary[key] = value
+        return
+
+    suffixNum = 1
+    while suffixNum != duplicateLimit:
+        newKey = f"{key}_{suffixNum}"
+        if newKey not in dictionary:
+            dictionary[newKey] = value
+            return
+        suffixNum += 1
+
+def loadFromJson(path):
+    with open(path) as fp:
+        return json.load(fp)
+
+def getMatches(location, regex, user="", password=""):
+    if not user:
+        rawHTML = requests.get(location)
+    else:
+        rawHTML = requests.get(location, auth=HTTPBasicAuth(user, password))
+
+    soup = BeautifulSoup(rawHTML.text, 'html.parser')
+    exp = re.compile(regex)
+
+    folders = []
+    matches = []
+    for link in soup.find_all('a'):
+        link = link.get('href')
+
+        if link is None:
+            continue
+        
+        fullLink = urljoin(location, link)
+        if fullLink.startswith(location) and fullLink != location and fullLink.endswith('/'): # Folder classification
+            folders.append(fullLink)
+
+        if exp.match(link):
+            matches.append(fullLink)
+
+    return folders, matches
+
+def crawl(url, regexMatch, maxDepth=0, user="", password=""):
+    subDirDepth = 0
+    folders = [url]
+    matchingFiles = []
+
+    while len(folders):
+        print(f"At depth: {subDirDepth}", end="\r")
+        newFolders = []
+
+        for folder in folders:
+            newSubFolders, newFiles = getMatches(folder, regexMatch, user, password)
+            matchingFiles.extend(newFiles)
+
+            if subDirDepth < maxDepth:
+                newFolders.extend(newSubFolders)
+            
+        folders = newFolders.copy()
+        subDirDepth += 1
+
+    return matchingFiles
---
+++ src/lib/dataframeFuncs.py
@@ -0,0 +1,110 @@
+import logging
+import pandas as pd
+import numpy as np
+from lib.commonFuncs import reverseLookup
+
+def getColumnEmptyCount(df, column):
+    if column not in df.columns:
+        # return len(df.index)
+        return -1
+
+    return len(df[df[column].isna()])
+
+def getColumnCount(df, column):
+    if column not in df.columns:
+        return -1
+
+    return len(df[df[column].notna()])
+
+def cleanColName(colName):
+    stripChars = "# _"
+    for c in stripChars:
+        colName = colName.strip(c)
+    return colName
+
+def createMappings(columns, dwcLookup, customLookup, prefix, preserveDwCMatch=False, keepMapFields=[], skipRemap=[]):
+    newColumns = {}
+    copyColumns = {}
+
+    reverseDwCLookup = reverseLookup(dwcLookup)
+    reverseCustomLookup = reverseLookup(customLookup)
+
+    for currentCol in columns:
+        if currentCol in skipRemap:
+            newColumns[currentCol] = currentCol
+            continue
+
+        prefixedCleanColName = f"{prefix}_{cleanColName(currentCol)}"
+
+        if currentCol in reverseCustomLookup: # Apply custom mappings
+            newColumns[currentCol] = reverseCustomLookup[currentCol]
+
+        if currentCol in reverseDwCLookup: # Apply dwc map
+            newColumns[currentCol] = reverseDwCLookup[currentCol]
+
+        if currentCol not in newColumns: # Not mapped yet from previous maps
+            if currentCol in dwcLookup and preserveDwCMatch: # If column is already a valid dwc field and keep matches
+                copyColumns[f"{prefix}_{currentCol}"] = currentCol
+
+            elif currentCol in dwcLookup or currentCol in customLookup: # If it exists in either set of keys
+                newColumns[currentCol] = currentCol # Don't remap, it's a field we want
+
+            else: # Column isn't in either map
+                newColumns[currentCol] = prefixedCleanColName # Prefix columns that have no available mapping
+
+        if currentCol in newColumns and newColumns[currentCol] in keepMapFields: # If the original column of a mapped field should be kept
+            copyColumns[currentCol] = prefixedCleanColName
+
+    return newColumns, copyColumns
+
+def applyColumnMap(df, newColumns, copyColumns, verbose=False):
+    # Copy columns as per column map
+    for oldCol, newCol in copyColumns.items():
+        df[newCol] = df[oldCol]
+
+        if verbose:
+            print(f"Created duplicate of {oldCol} named {newCol}")
+
+    # Rename columns with the new columns map
+    df = df.rename(newColumns, axis=1)
+
+    if verbose:
+        for oldCol, newCol in newColumns.items():
+            print(f"Mapped {oldCol} to {newCol}")
+
+    return df
+
+def mapAndApply(df, dwcLookup, customLookup, prefix, preserveDwCMatch=False, keepMapFields=[]):
+    newColumns, copyColumns = createMappings(df.columns, dwcLookup, customLookup, prefix, preserveDwCMatch, keepMapFields)
+    return applyColumnMap(df, newColumns, copyColumns)
+
+def splitField(df, column, func, newColumns):
+    seriesList = zip(*df[column].apply(lambda x: func(x)))
+    for colname, series in zip(newColumns, seriesList):
+        if column is None:
+            continue
+        
+        df[colname] = pd.Series(series, dtype=object).replace('', np.nan)
+
+    df = df.drop(column, axis=1)
+
+    return df
+
+def dropEmptyColumns(df, verbose=False):
+    remainingColumns = set(df.columns)
+    df.dropna(how='all', axis=1, inplace=True) # Drop all completely empty columns
+
+    if verbose:
+        for column in remainingColumns.symmetric_difference(df.columns): # Iterate over removed column names
+            print(f"Dropped empty column {column}")
+
+    return df
+
+def applyExclusions(df, exclusionMap):
+    for excludeType, properties in exclusionMap.items():
+        dwcName = properties["dwc"]
+        exclusions = properties["data"]
+        if dwcName in df.columns: # Make sure dwc field exists in file
+            df = df.drop(df[df[dwcName].isin(exclusions)].index)
+    
+    return df
