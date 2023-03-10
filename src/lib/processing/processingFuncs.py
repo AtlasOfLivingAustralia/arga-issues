be115717b6e215b7277a118261177232ea45e4c1
Christopher Mangion <christopher.mangion@hotmail.com>
2023-03-10 16:17:22 +1000

    Improved importPath generation to work with all path types

--- src/lib/processing/processingFuncs.py
+++ src/lib/processing/processingFuncs.py
@@ -3,9 +3,9 @@ import importlib
 
 def importModule(path: str):
     scriptPath = Path(path)
-    pathString = str(scriptPath.parent / scriptPath.stem)
-    pathString = pathString.replace("\\", ".")
-    return importlib.import_module(pathString)
+    pathParts = scriptPath.parts[:-1] + (scriptPath.stem,) # Parts is tuple, concat with tuple only
+    importPath = ".".join(pathParts)
+    return importlib.import_module(importPath)
 
 def importFunction(path: str, func: str):
     module = importModule(path)
