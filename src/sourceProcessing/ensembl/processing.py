2bf5dc4d6cfcf13cf435e41680297b11638b817e
Christopher Mangion <christopher.mangion@hotmail.com>
2023-05-03 15:11:55 +1000

    Added ensembl-species

--- src/sourceProcessing/ensembl/processing.py
+++ src/sourceProcessing/ensembl/processing.py
@@ -1,9 +1,22 @@
 import json
 import pandas as pd
+import requests
 
 def convert(stageFile, outputFilePath):
     with open(stageFile.filePath) as fp:
         data = json.load(fp)
 
     df = pd.DataFrame.from_records(data)
+    df.to_csv(outputFilePath, index=False)
+
+def speciesDownload(outputFilePath):
+    url = "https://rest.ensembl.org/info/species?"
+    request = requests.get(url, headers={ "Content-Type" : "application/json"})
+ 
+    if not request.ok:
+        request.raise_for_status()
+        return
+    
+    data = request.json()
+    df = pd.DataFrame.from_records(data["species"])
     df.to_csv(outputFilePath, index=False)
