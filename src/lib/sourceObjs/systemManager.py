cfdaf550cadbebeca4b21efa07a88563ff37731d
Christopher Mangion <christopher.mangion@hotmail.com>
2023-05-09 13:04:07 +1000

    Fixed incorrect imports

--- src/lib/sourceObjs/systemManager.py
+++ src/lib/sourceObjs/systemManager.py
@@ -1,5 +1,6 @@
 from pathlib import Path
-from lib.processing.stages import FileStage, StageFile, StageDownloadScript, StageScript, StageDWCConversion
+from lib.processing.stageFile import StageFileStep, StageFile
+from lib.processing.stageScript import StageDownloadScript, StageScript, StageDWCConversion
 from lib.processing.parser import SelectorParser
 from lib.processing.dwcProcessor import DWCProcessor
 
@@ -29,12 +30,12 @@ class SystemManager:
         self.parser = SelectorParser(self.rootDir, self.downloadDir, self.processingDir, self.preConversionDir, self.dwcDir)
         self.dwcProcessor = DWCProcessor(self.location, self.dwcProperties, self.enrichDBs, self.dwcDir)
 
-        self.stages = {stage: [] for stage in FileStage}
+        self.stages = {stage: [] for stage in StageFileStep}
 
-    def getFiles(self, stage: FileStage):
+    def getFiles(self, stage: StageFileStep):
         return self.stages[stage]
     
-    def create(self, stage: FileStage, fileNumbers: list[int] = [], overwrite: int = 0):
+    def create(self, stage: StageFileStep, fileNumbers: list[int] = [], overwrite: int = 0):
         if not fileNumbers: # Create all files:
             for file in self.stages[stage]:
                 file.create(stage, overwrite)
@@ -46,11 +47,11 @@ class SystemManager:
             else:
                 print(f"Invalid number provided: {number}")
 
-    def buildProcessingChain(self, processingSteps: list[dict], initialInputs: list[StageFile], finalStage: FileStage) -> None:
+    def buildProcessingChain(self, processingSteps: list[dict], initialInputs: list[StageFile], finalStage: StageFileStep) -> None:
         inputs = initialInputs.copy()
         for idx, step in enumerate(processingSteps):
             scriptStep = StageScript(step, inputs, self.parser)
-            stage = FileStage.INTERMEDIATE if idx < len(processingSteps) else finalStage
+            stage = StageFileStep.INTERMEDIATE if idx < len(processingSteps) else finalStage
             inputs = [StageFile(filePath, {}, scriptStep, stage) for filePath in scriptStep.getOutputs()]
 
         self.stages[finalStage].extend(inputs)
@@ -59,30 +60,30 @@ class SystemManager:
         downloadedFile = self.downloadDir / fileName # downloaded files go into download directory
         downloadScript = StageDownloadScript(url, downloadedFile, self.parser, self.user, self.password)
         
-        rawFile = StageFile(downloadedFile, {} if processing else fileProperties, downloadScript, FileStage.RAW)
-        self.stages[FileStage.RAW].append(rawFile)
+        rawFile = StageFile(downloadedFile, {} if processing else fileProperties, downloadScript, StageFileStep.RAW)
+        self.stages[StageFileStep.RAW].append(rawFile)
 
-        self.buildProcessingChain(processing, [rawFile], FileStage.PROCESSED)
+        self.buildProcessingChain(processing, [rawFile], StageFileStep.PROCESSED)
 
     def addRetrieveScriptStage(self, script, processing, fileProperties):
         scriptStep = StageScript(script, [], self.parser)
-        outputs = [StageFile(filePath, fileProperties, scriptStep, FileStage.RAW) for filePath in scriptStep.getOutputs()]
-        self.stages[FileStage.RAW].extend(outputs)
+        outputs = [StageFile(filePath, fileProperties, scriptStep, StageFileStep.RAW) for filePath in scriptStep.getOutputs()]
+        self.stages[StageFileStep.RAW].extend(outputs)
 
-        self.buildProcessingChain(processing, outputs, FileStage.PROCESSED)
+        self.buildProcessingChain(processing, outputs, StageFileStep.PROCESSED)
     
     def addCombineStage(self, processing):
-        self.buildProcessingChain(processing, self.stages[FileStage.PROCESSED], FileStage.COMBINED)
+        self.buildProcessingChain(processing, self.stages[StageFileStep.PROCESSED], StageFileStep.COMBINED)
     
     def pushPreDwC(self):
-        fileStages = (FileStage.RAW, FileStage.PROCESSED, FileStage.COMBINED, FileStage.PRE_DWC)
+        fileStages = (StageFileStep.RAW, StageFileStep.PROCESSED, StageFileStep.COMBINED, StageFileStep.PRE_DWC)
         for idx, stage in enumerate(fileStages[:-1], start=1):
             nextStage = fileStages[idx]
             if self.stages[stage] and not self.stages[nextStage]: # If this stage has files and next doesn't
                 self.stages[nextStage] = self.stages[stage].copy()
 
-        for file in self.stages[FileStage.PRE_DWC]:
+        for file in self.stages[StageFileStep.PRE_DWC]:
             conversionScript = StageDWCConversion(file, self.dwcProcessor)
             dwcOutput = conversionScript.getOutput()
-            convertedFile = StageFile(dwcOutput, {}, conversionScript, FileStage.DWC)
-            self.stages[FileStage.DWC].append(convertedFile)
+            convertedFile = StageFile(dwcOutput, {}, conversionScript, StageFileStep.DWC)
+            self.stages[StageFileStep.DWC].append(convertedFile)
