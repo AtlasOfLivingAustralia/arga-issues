7e2f580c263a01e32ae81c3152a030bb9d2c83f9
Christopher Mangion <christopher.mangion@hotmail.com>
2023-06-09 11:20:44 +1000

    Removed enrichment steps in DWC processing

--- src/lib/processing/dwcProcessor.py
+++ src/lib/processing/dwcProcessor.py
@@ -10,10 +10,9 @@ class DWCProcessor:
     customLookup = cmn.loadFromJson(cfg.filePaths.otherMapping)
     exclude = cmn.loadFromJson(cfg.filePaths.excludedEntries)
 
-    def __init__(self, prefix: str, dwcProperties: dict, enrichDBs: dict, outputDir: Path):
+    def __init__(self, prefix: str, dwcProperties: dict, outputDir: Path):
         self.prefix = prefix
         self.dwcProperties = dwcProperties
-        self.enrichDBs = enrichDBs
         self.outputDir = outputDir
 
         self.augments = dwcProperties.pop("augment", [])
@@ -39,35 +38,15 @@ class DWCProcessor:
             df = dff.applyColumnMap(df, newColMap, copyColMap)
             df = dff.applyExclusions(df, self.exclude)
             df = self.applyAugments(df)
-            df = self.applyEnrichment(df)
             # df = dff.dropEmptyColumns(df)
 
             self.writer.writeDF(df)
 
         self.writer.oneFile(outputFilePath)
 
-    def checkPreparedEnrichment(self):
-        for database in self.enrichDBs.values():
-            for enrichFile in database.getDWCFiles():
-                if not enrichFile.filePath.exists():
-                    print(f"Database {database.database} file {enrichFile.filePath} not prepared for enrichment, cancelling DWC conversion")
-                    return False
-        return True
-
     def applyAugments(self, df):
         for augment in self.augmentSteps:
             df = augment.process(df)
-        return df
-    
-    def applyEnrichment(self, df):
-        for keyword, database in self.enrichDBs.items():
-            for enrichFile in database.getDWCFiles():
-                for enrichChunk in dff.chunkGenerator(enrichFile.filePath, self.chunkSize, enrichFile.separator, enrichFile.firstRow, enrichFile.encoding):
-                    if keyword not in df or keyword not in enrichChunk:
-                        continue
-
-                    columnDifferences = list(enrichChunk.columns.difference(df.columns)) + [keyword]
-                    df = df.merge(enrichChunk[columnDifferences], 'left', on=keyword)
         return df
 
 class Augment:
