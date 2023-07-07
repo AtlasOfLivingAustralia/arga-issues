0ca84cd23e87373776e2327b1326799a60e5ce2b
Christopher Mangion <christopher.mangion@hotmail.com>
2023-07-07 11:20:32 +1000

    ViewDWC now has unique flags for showing mapped or unmapped fields only

--- src/tools/source/viewDwC.py
+++ src/tools/source/viewDwC.py
@@ -4,7 +4,9 @@ import pandas as pd
 if __name__ == '__main__':
     parser = SourceArgParser(description="View portion of DWC file")
     parser.add_argument("-e", "--entries", type=int, default=100, help="Amount of entries to view")
-    parser.add_argument("-x", "--excludeUnmapped", action="store_true", help="Exclude unmapped fields")
+    columnGroup = parser.add_mutually_exclusive_group()
+    columnGroup.add_argument("-m", "--mapped", action="store_true", help="Get only mapped fields")
+    columnGroup.add_argument("-u", "--unmapped", action="store_true", help="Get only unmapped fields")
     
     sources, args = parser.parse_args()
     for source in sources:
@@ -15,9 +17,12 @@ if __name__ == '__main__':
                 continue
 
             df = next(file.loadDataFrameIterator(args.entries))
-
-            if args.excludeUnmapped:
-                keepColls = [col for col in df.columns if not col.startswith(source.location)]
-                df = df[keepColls]
+            
+            if args.mapped:
+                unmapped = [col for col in df.columns if not col.startswith(source.location)]
+                df = df[unmapped]
+            elif args.unmapped:
+                mapped = [col for col in df.columns if col.startswith(source.location)]
+                df = df[mapped]
 
             df.to_csv(file.directory.parent / f"{file.filePath.name}_example.csv", index=False)
