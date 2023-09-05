315623e598cbf9806cfe75c7ed2af42f54162129
Christopher Mangion <christopher.mangion@hotmail.com>
2023-09-05 12:12:37 +1000

    Fixed an issue with empty values existing as None Now collect all images by iterating over all pages in api

--- src/tools/imageCollector/locations/vicMuseum.py
+++ src/tools/imageCollector/locations/vicMuseum.py
@@ -1,9 +1,15 @@
 import requests
-import json
 import pandas as pd
+import math
 
 def buildURL(keyword: str, perPage: int, page: int = 1) -> str:
     return f"https://collections.museumsvictoria.com.au/api/{keyword}?perpage={perPage}&page={page}&hasimages=yes"
+
+def getAndCheck(propertyDict: dict, property: str, default: any) -> any:
+    value = propertyDict.get(property, default)
+    if value is None and default is not None:
+        return default
+    return value
 
 def processEntry(entry: dict) -> list[dict]:
     media = entry["media"]
@@ -16,35 +22,36 @@ def processEntry(entry: dict) -> list[dict]:
         if not mediaObject["type"] == "image":
             continue
 
-        
         for size in ("large", "medium", "small", "thumbnail"):
-            image = mediaObject.get("large", {})
+            image = getAndCheck(mediaObject, size, {})
             if image:
                 break
         else:
             continue
 
         imageFormat = image["uri"].rsplit(".", 1)[-1]
+        caption = getAndCheck(mediaObject, "caption", "")
+        caption = caption.replace("<em>", "").replace("</em>", "")
 
         info = {
             "type": "image",
             "format": imageFormat,
             "identifier": image.get("uri", ""),
             "references": f"https://collections.museumsvictoria.com.au/{speciesID}",
-            "title": mediaObject.get("alternativeText", ""),
-            "description": mediaObject.get("caption", "").replace("<em>", "").replace("</em>", ""),
-            "created": mediaObject.get("dateModiied", ""),
-            "creator": mediaObject.get("creators", ""),
+            "title": getAndCheck(mediaObject, "alternativeText", ""),
+            "description": caption,
+            "created": getAndCheck(mediaObject, "dateModiied", ""),
+            "creator": getAndCheck(mediaObject, "creators", ""),
             "contributor": "",
             "publisher": "Museums Victoria",
             "audience": "",
-            "source": mediaObject.get("sources", ""),
-            "license": mediaObject.get("licence", {}).get("name", ""),
-            "rightsHolder": mediaObject.get("rightsStatement", ""),
-            "datasetID": mediaObject.get("id", ""),
+            "source": getAndCheck(mediaObject, "sources", ""),
+            "license": getAndCheck(mediaObject, "licence", {}).get("name", ""),
+            "rightsHolder": getAndCheck(mediaObject, "rightsStatement", ""),
+            "datasetID": getAndCheck(mediaObject, "id", ""),
             "taxonName": taxonName,
-            "width": image.get("width", 0),
-            "height": image.get("height", 0)
+            "width": getAndCheck(image, "width", 0),
+            "height": getAndCheck(image, "height", 0)
         }
 
         images.append(info)
@@ -53,21 +60,24 @@ def processEntry(entry: dict) -> list[dict]:
 
 def run():
     keywords = ["species", "specimens"]
-    entriesPerPage = 1000
+    entriesPerPage = 100
 
     headers = {
         "User-Agent": ""
     }
 
-    response = requests.get(buildURL("species", 100), headers=headers)
-    data = response.json()
-
+    response = requests.get(buildURL("species", 1), headers=headers)
     totalResults = int(response.headers.get("Total-Results", 0))
-    print(totalResults)
+    totalCalls = math.ceil(totalResults / entriesPerPage)
 
     entries = []
-    for entry in data:
-        entries.extend(processEntry(entry))
+    for call in range(1, totalCalls+1):
+        print(f"At call: {call} / {totalCalls}", end="\r")
+        response = requests.get(buildURL("species", entriesPerPage, call), headers=headers)
+        data = response.json()
+
+        for entry in data:
+            entries.extend(processEntry(entry))
 
     df = pd.DataFrame.from_records(entries)
     df.to_csv("images.csv", index=False)
