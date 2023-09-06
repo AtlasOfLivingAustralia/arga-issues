54140b25ec03f9948c6b479b3edee6a205a57461
Christopher Mangion <christopher.mangion@hotmail.com>
2023-09-06 15:22:36 +1000

    Added multithreading to flickr requests Added a bit more feedback from the script while running

--- src/tools/imageCollector/locations/flickr.py
+++ src/tools/imageCollector/locations/flickr.py
@@ -1,7 +1,8 @@
 import requests
 import json
 import math
-from datetime import datetime
+import pandas as pd
+import concurrent.futures
 
 def buildURL(apiKey, method, **kwargs):
     baseURL = "https://api.flickr.com/services/rest/?method="
@@ -14,25 +15,72 @@ def buildURL(apiKey, method, **kwargs):
 
     return f"{url}{suffix}"
 
+def processPhoto(apiKey: str, licenses: dict, photo: dict) -> dict:
+    photoID = photo["id"]
+
+    response = requests.get(buildURL(apiKey, "flickr.photos.getInfo", photo_id=photoID))
+    if response.status_code != 200:
+        print(f"Info error with id: {photoID}")
+        return {}
+    
+    photoInfo = response.json()["photo"]
+
+    response = requests.get(buildURL(apiKey, "flickr.photos.getSizes", photo_id=photoID))
+    if response.status_code != 200:
+        print(f"Size error with id: {photoID}")
+        return {}
+    
+    photoSizes = response.json()["sizes"]
+    image = sorted(photoSizes["size"], key=lambda x: int(x["width"]) * int(x["height"]), reverse=True)[0]
+    
+    tags = [tag["raw"] for tag in photoInfo["tags"]["tag"]]
+
+    return {
+        "type": "image",
+        "format": image["source"].rsplit(".", 1)[-1],
+        "identifier": image["source"],
+        "references": image["url"].rsplit("/", 3)[0],
+        "title": photoInfo["title"]["_content"],
+        "description": photoInfo["description"]["_content"],
+        "created": photoInfo["dates"]["taken"],
+        "creator": photoInfo["owner"]["username"],
+        "contributor": "",
+        "publisher": photoInfo["owner"]["username"],
+        "audience": "",
+        "source": "flickr.com",
+        "license": licenses[int(photoInfo["license"])],
+        "rightsHolder": "",
+        "datasetID": photoID,
+        "taxonName": "",
+        "width": image["width"],
+        "height": image["height"],
+        "tags": tags
+    }
+
 def run():
     with open("flickrkey.txt") as fp:
-        apiKey = fp.read()
+        apiKeyData = fp.read()
 
     with open("flickrusers.txt") as fp:
         users = fp.read()
 
-    key, secret = apiKey.rstrip("\n").split("\n")
+    apiKey, secret = apiKeyData.rstrip("\n").split("\n")
     userList = users.rstrip("\n").split("\n")
 
     photosPerCall = 500
 
     # Get licenses
-    response = requests.get(buildURL(key, "flickr.photos.licenses.getInfo"))
+    print("Getting license information")
+    response = requests.get(buildURL(apiKey, "flickr.photos.licenses.getInfo"))
     licenseData = response.json()
     licenses = {licenseInfo["id"]: licenseInfo["name"] for licenseInfo in licenseData["licenses"]["license"]}
 
     for user in userList:
-        response = requests.get(buildURL(key, "flickr.people.getPhotos", user_id=user, per_page=1))
+        if user.startswith("_"):
+            continue
+
+        print(f"Getting photos for {user}")
+        response = requests.get(buildURL(apiKey, "flickr.people.getPhotos", user_id=user, per_page=1))
         data = response.json()
 
         totalPhotos = data["photos"]["total"]
@@ -40,46 +88,30 @@ def run():
 
         photos = []
         for call in range(1, totalCalls + 1):
-            response = requests.get(buildURL(key, "flickr.people.getPhotos", user_id=user, per_page=photosPerCall, page=call))
+            print(f"Processing page {call} / {totalCalls}")
+            response = requests.get(buildURL(apiKey, "flickr.people.getPhotos", user_id=user, per_page=photosPerCall, page=call))
             photoData = response.json()["photos"]
+            photoList = photoData.get("photo", [])
 
-            for photo in photoData.get("photo", []):
-                photoID = photo["id"]
+            with concurrent.futures.ThreadPoolExecutor(max_workers=50) as executor:
+                futures = (executor.submit(processPhoto, apiKey, licenses, photo) for photo in photoList)
+            
+                try:
+                    for idx, future in enumerate(concurrent.futures.as_completed(futures), start=1):
+                        result = future.result()
+                        print(f"At photo {idx} / {len(photoList)}", end="\r")
+                        if result:
+                            photos.append(result)
 
-                response = requests.get(buildURL(key, "flickr.photos.getInfo", photo_id=photoID))
-                photoInfo = response.json()["photo"]
+                except (KeyboardInterrupt, ValueError):
+                    print("\nExiting...")
+                    executor.shutdown(cancel_futures=True)
+                    exit()
 
-                response = requests.get(buildURL(key, "flickr.photos.getSizes", photo_id=photoID))
-                photoSizes = response.json()["sizes"]
+            print()
 
-                photo = sorted(photoSizes["size"], key=lambda x: x["width"] * x["height"], reverse=True)[0]
-
-                info = {
-                    "type": "image",
-                    "format": photo["source"].rsplit(".", 1)[-1],
-                    "identifier": photo["source"],
-                    "references": photo["url"].rsplit("/", 3)[0],
-                    "title": photoInfo["title"]["_content"],
-                    "description": photoInfo["description"]["_content"],
-                    "created": photoInfo["dates"]["taken"],
-                    "creator": photoInfo["owner"]["username"],
-                    "contributor": "",
-                    "publisher": photoInfo["owner"]["username"],
-                    "audience": "",
-                    "source": "flickr.com",
-                    "license": licenses[int(photoInfo["license"])],
-                    "rightsHolder": "",
-                    "datasetID": photoID,
-                    "taxonName": "",
-                    "width": photo["width"],
-                    "height": photo["height"]
-                }
-
-                photos.append(info)
-
-        
+        df = pd.DataFrame.from_records(photos)
+        df.to_csv(f"{user}_flickrImages.csv", index=False)
 
 if __name__ == "__main__":
     run()
-
-    a = datetime.fromtimestamp()
