a4f69b68d824831a3cb2d548a3dd3514cca68e52
Christopher Mangion <christopher.mangion@hotmail.com>
2023-09-07 20:20:50 +1000

    Added proper image identifier from photo_id Added proper license text from license name (CC0 needs work) All relevant columns renamed properly Added some missing columns with set values Now write to a single file

--- src/tools/imageCollector/locations/inaturalist/inaturalist.py
+++ src/tools/imageCollector/locations/inaturalist/inaturalist.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import pandas as pd
 import lib.dataframeFuncs as dff
 from lib.tools.subfileWriter import Writer
+import numpy as np
 
 def run():
     dataFolder = Path("./inaturalist-open-data-20230827")
@@ -15,50 +16,58 @@ def run():
     taxonomy = pd.read_csv(taxa, dtype=object, sep="\t")
     taxonomy.drop(taxonomy[taxonomy["rank"] != "species"].index, inplace=True)
     taxonomy.drop(["ancestry", "rank_level", "rank", "active"], axis=1, inplace=True)
+    taxonomy.to_csv("taxonomy.csv", index=False)
 
     # Prepare observers for getting creator name
     observers = pd.read_csv(observers, dtype=object, sep="\t")
     observers["creator"] = observers["name"].fillna(observers["login"])
     observers.drop(["name", "login"], axis=1, inplace=True)
 
-    writer = Writer(Path("."), "chunks", "chunk")
+    writer = Writer(Path("."), "subfiles", "chunk")
 
-    photosGen = dff.chunkGenerator(photos, 100, "\t")
+    photosGen = dff.chunkGenerator(photos, 1024*1024*4, "\t")
     for idx, df in enumerate(photosGen, start=1):
         df.drop(df[df["license"] == "CC-BY-NC-ND"].index, inplace=True)
 
-        df["taxon_id"] = "" # Add empty taxon_id column for filling with observations
-        obsvGen = dff.chunkGenerator(observations, 1024*1024, "\t")
+        df["extension"] = df["extension"].str.lower()
+        df["identifier"] = "https://inaturalist-open-data.s3.amazonaws.com/photos/" + df["photo_id"] + "/original." + df["extension"]
+ 
+        # Add empty taxon_id and observed_on columns for filling with observations
+        df["taxon_id"] = np.NaN
+        df["observed_on"] = np.NaN
 
-        print(" "*50, end="\r")
+        print(" "*50, end="\r") # Clear stdout
+        obsvGen = dff.chunkGenerator(observations, 1024*1024, "\t")        
         for subIdx, obsv in enumerate(obsvGen, start=1):
             print(f"At: chunk {idx} | sub chunk {subIdx}", end="\r")
             obsv.drop(obsv[obsv["quality_grade"] != "research"].index, inplace=True)
-            obsv = obsv[["observation_uuid", "taxon_id"]] # Only columns needed
-            # obsv.drop(["latitude" , "longitude", "positional_accuracy", "quality_grade", "observer_id", "observed_on"], axis=1, inplace=True)
-            df["taxon_id"] = (df.set_index("observation_uuid")["taxon_id"].fillna(obsv.set_index("observation_uuid")["taxon_id"]).reset_index(drop=True))
-            break
+            obsv.drop(["latitude" , "longitude", "positional_accuracy", "quality_grade", "observer_id"], axis=1, inplace=True)
 
-        df.to_csv("before.csv", index=False)
+            for column in ("taxon_id", "observed_on"):
+                df[column] = (df.set_index("observation_uuid")[column].fillna(obsv.set_index("observation_uuid")[column]).reset_index(drop=True))
+
         df = pd.merge(df, taxonomy, "left", on="taxon_id")
         df = pd.merge(df, observers, "left", on="observer_id")
-        df.to_csv("after.csv", index=False)
-        break
-        # df.drop(["position", "taxon_id", "observer_id", "observation_uuid"], axis=1, inplace=True)
+
+        df.drop(["position", "taxon_id", "observer_id", "observation_uuid", "photo_id"], axis=1, inplace=True)
+        df["license"] = "©" + df["creator"] + ", some rights reserved (" + df["license"] + ")"
+
+        # Renaming fields
+        df.rename({
+            "extension": "format",
+            "photo_uuid": "datasetID",
+            "name": "taxonName",
+            "observed_on": "created"
+        }, axis=1, inplace=True)
+
+        df["type"] = "image"
+        df["source"] = "iNaturalist"
+        df["publisher"] = "iNaturalist"
         
-        # # Renaming fields
-        # df.rename({
-        #     "extension": "format",
-        #     "photo_uuid": "datasetID"
-        #     "taxon_"
-        # })
-        # df["type"] = "image"
-        
-        # # Missing fields:
-        
-        # writer.writeDF(df)
+        writer.writeDF(df)
     
     print()
+    writer.oneFile("inaturalist.csv")
 
 if __name__ == "__main__":
     # downloadURL = "https://inaturalist-open-data.s3.amazonaws.com/metadata/inaturalist-open-data-latest.tar.gz"
