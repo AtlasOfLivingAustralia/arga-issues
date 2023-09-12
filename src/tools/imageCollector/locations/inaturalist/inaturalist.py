cc1a06edcefdc22c9ca4b28a16b2e987cb16caff
Christopher Mangion <christopher.mangion@hotmail.com>
2023-09-13 01:05:52 +1000

    Now use BigFileWriter Fixed an issue with constantly resetting the index of dataframe Fixed an issue merging on taxon_id Fixed an issue where a column was being dropped too early

--- src/tools/imageCollector/locations/inaturalist/inaturalist.py
+++ src/tools/imageCollector/locations/inaturalist/inaturalist.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import pandas as pd
 import lib.dataframeFuncs as dff
-from lib.tools.subfileWriter import Writer
+from lib.tools.bigFileWriter import BigFileWriter
 import numpy as np
 
 def run():
@@ -12,46 +12,62 @@ def run():
     photos = dataFolder / "photos.csv" # Large
     taxa = dataFolder / "taxa.csv"
 
+    photoIDs = Path("./photoIDs.txt")
+
     # Prepare taxonomy for getting species name
     taxonomy = pd.read_csv(taxa, dtype=object, sep="\t")
     taxonomy.drop(taxonomy[taxonomy["rank"] != "species"].index, inplace=True)
     taxonomy.drop(["ancestry", "rank_level", "rank", "active"], axis=1, inplace=True)
+    taxonomy["taxon_id"] = taxonomy["taxon_id"].astype(int)
 
     # Prepare observers for getting creator name
     observers = pd.read_csv(observers, dtype=object, sep="\t")
     observers["creator"] = observers["name"].fillna(observers["login"])
     observers.drop(["name", "login"], axis=1, inplace=True)
 
-    writer = Writer(Path("."), "subfiles", "chunk")
+    writer = BigFileWriter(Path("./inaturalist.csv"), "subfiles")
 
     photosGen = dff.chunkGenerator(photos, 1024*1024*2, "\t")
     for idx, df in enumerate(photosGen, start=1):
         df.drop(df[df["license"] == "CC-BY-NC-ND"].index, inplace=True)
         df.drop_duplicates("photo_uuid", inplace=True)
+        df.drop_duplicates("observation_uuid", inplace=True)
+
+        with open(photoIDs) as fp:
+            ids = fp.read().split("\n")[:-1]
+
+        df = df[df["photo_uuid"].isin(ids)] # Filter based on accepted photo uuids
+        del ids
  
         # Add empty taxon_id and observed_on columns for filling with observations
         df["taxon_id"] = np.NaN
         df["observed_on"] = np.NaN
 
-        print(" "*50, end="\r") # Clear stdout
+        df.set_index("observation_uuid", inplace=True)
+
+        print(" "*100, end="\r") # Clear stdout
         obsvGen = dff.chunkGenerator(observations, 1024*1024, "\t")        
         for subIdx, obsv in enumerate(obsvGen, start=1):
             print(f"At: chunk {idx} | sub chunk {subIdx}", end="\r")
             obsv.drop(obsv[obsv["quality_grade"] != "research"].index, inplace=True)
             obsv.drop(["latitude" , "longitude", "positional_accuracy", "quality_grade", "observer_id"], axis=1, inplace=True)
+            obsv.drop_duplicates("observation_uuid", inplace=True)
+            obsv.set_index("observation_uuid", inplace=True)
 
             for column in ("taxon_id", "observed_on"):
-                df[column] = (df.set_index("observation_uuid")[column].fillna(obsv.set_index("observation_uuid")[column]).reset_index(drop=True))
-
-        df["taxon_id"] = df["taxon_id"].astype(object) # Fixes an issue where taxon_id is sometimes float
+                df[column].fillna(obsv[column], inplace=True)
+        
+        df.reset_index(drop=True, inplace=True) # Reset index and drop observation uuid
+        df.drop(df[df["taxon_id"].isna()].index, inplace=True) # Remove NaN entries to allow conversion to int
+        df["taxon_id"] = df["taxon_id"].astype(int) # Fixes an issue where taxon_id is sometimes float
 
         df = pd.merge(df, taxonomy, "left", on="taxon_id")
         df = pd.merge(df, observers, "left", on="observer_id")
-        
-        df.drop(["position", "taxon_id", "observer_id", "observation_uuid", "photo_id"], axis=1, inplace=True)
-        
+
         df["license"] = "© " + df["creator"] + ", some rights reserved (" + df["license"] + ")"
         df["identifier"] = "https://inaturalist-open-data.s3.amazonaws.com/photos/" + df["photo_id"] + "/original." + df["extension"]
+
+        df.drop(["position", "taxon_id", "observer_id", "photo_id"], axis=1, inplace=True)
 
         # Renaming fields
         df.rename({
@@ -68,7 +84,7 @@ def run():
         writer.writeDF(df)
     
     print()
-    writer.oneFile(Path("./inaturalist.csv"))
+    writer.oneFile(False)
 
 if __name__ == "__main__":
     # downloadURL = "https://inaturalist-open-data.s3.amazonaws.com/metadata/inaturalist-open-data-latest.tar.gz"
