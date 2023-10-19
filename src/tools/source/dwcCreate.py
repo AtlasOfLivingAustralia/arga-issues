a4aeb0313b71873a7f81d00081ed604e0a2e4113
Christopher Mangion <christopher.mangion@hotmail.com>
2023-10-19 10:29:07 +1000

    Now use BigFileWriter instead of SubFileWriter Separate DWC files WIP

--- src/tools/source/dwcCreate.py
+++ src/tools/source/dwcCreate.py
@@ -3,6 +3,7 @@ from lib.processing.stageFile import StageFileStep
 
 if __name__ == '__main__':
     parser = SourceArgParser(description="Convert preDWC file to DWC")
+    parser.add_argument("-s", "--singlefile", action="store_true", help="Create single DwC file")
     
     sources, selectedFiles, args = parser.parse_args()
     for source in sources:
