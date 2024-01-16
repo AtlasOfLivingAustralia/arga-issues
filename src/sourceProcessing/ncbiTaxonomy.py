32767e73987b510f81c3afb271c4c354f20857c8
Christopher Mangion <christopher.mangion@hotmail.com>
2024-01-17 09:58:34 +1000

    Updated ncbiTaxonomy with less strings, and added species name to output Final file is now made in final processing, no as part of the downloaded file processing in config

--- src/sourceProcessing/ncbiTaxonomy.py
+++ src/sourceProcessing/ncbiTaxonomy.py
@@ -1,121 +1,173 @@
-import pickle
-import pandas as pd
-import argparse
 from pathlib import Path
-from typing import Self
+import pandas as pd
+from enum import Enum
+from lib.tools.logger import logger
 
-class Tree:
-    class Node:
-        def __init__(self, id: int):
-            self.id = id
-            self.parent = None
-            self.rank = "Unknown"
+class DumpFiles(Enum):
+    NODES = "nodes.dmp"
+    NAMES = "names.dmp"
+    DIVISIONS = "divisions.dmp"
+    GENETIC_CODES = "gencode.dmp"
+    DELETED_NODES = "delnodes.dmp"
+    MERGED_NODES = "merged.dmp"
+    CITATIONS = "citations.dmp"
 
-            self.name = ""
-            self.taxInfo = {}
-        
-        def setParent(self, parent: Self) -> None:
-            if self.id != parent.id:
-                self.parent = parent
+headings = {
+    DumpFiles.NODES: [
+        "tax_id",
+        "parent_tax_id",
+        "rank",
+        "embl_code",
+        "division_id",
+        "inherited_div_flag",
+        "genetic_code_id",
+        "inherited_GC_flag",
+        "mitochondrial_genetic_code_id",
+        "inherited_MGC_flag",
+        "GenBank_hidden_flag",
+        "hidden_subtree_root_flag",
+        "comments"
+    ],
+    DumpFiles.NAMES: [
+        "tax_id",
+        "name_txt",
+        "unique_name",
+        "name_class"
+    ],
+    DumpFiles.DIVISIONS: [
+        "division_id",
+        "division_cde",
+        "division_name",
+        "comments"
+    ],
+    DumpFiles.GENETIC_CODES: [
+        "genetic_code_id",
+        "abbreviation",
+        "name",
+        "cde",
+        "starts"
+    ],
+    DumpFiles.DELETED_NODES: [
+        "tax_id"
+    ],
+    DumpFiles.MERGED_NODES: [
+        "old_tax_id",
+        "new_tax_id",
+    ],
+    DumpFiles.CITATIONS: [
+        "cit_id",
+        "cit_key",
+        "pubmed_id",
+        "medline_id",
+        "url",
+        "text",
+        "taxid_list"
+    ]
+}
 
-        def setRank(self, rank: str) -> None:
-            self.rank = rank
+class TaxonNode:
+    def __init__(self, id: int):
+        self.id = id
+        self.parent = None
+        self.rank = "Unknown"
 
-        def setName(self, name: str) -> None:
-            self.name = name
+        self.name = ""
+        self.taxInfo = {}
+    
+    def setParent(self, parent: 'TaxonNode') -> None:
+        if self.id != parent.id:
+            self.parent = parent
 
-        def getTaxInfo(self, includeOwn: bool = False) -> dict[str, str]:
-            info = {}
-            if self.parent is not None:
-                info = self.parent.getTaxInfo(True)
+    def setRank(self, rank: str) -> None:
+        self.rank = rank
 
-            if includeOwn:
-                # info.append({"ID": self.id, "Rank": self.rank, "Name": self.name})
-                # info.append(f"{self.rank}: {self.name} ({self.id})")
-                info[self.rank] = self.name
+    def setName(self, name: str) -> None:
+        self.name = name
 
-            return info.copy()
+    def getTaxInfo(self, includeOwn: bool = False) -> dict[str, str]:
+        info = {}
+        if self.parent is not None:
+            info = self.parent.getTaxInfo(True)
 
+        if includeOwn:
+            info[self.rank] = self.name
+
+        return info.copy()
+
+class TaxonTree:
     def __init__(self):
-        self.nodes = {}
+        self.nodes: dict[int, TaxonNode] = {}
+        self.taxonRanks = [
+            "kingdom",
+            "phylum",
+            "class",
+            "order",
+            "family",
+            "subfamily",
+            "genus",
+            "subgenus",
+            "species"
+        ]
 
     def exists(self, taxID: int) -> bool:
         return taxID in self.nodes
 
     def addNode(self, taxID: int, parentTaxID: int, rank: str) -> None:
         if parentTaxID not in self.nodes:
-            self.nodes[parentTaxID] = self.Node(parentTaxID)
+            self.nodes[parentTaxID] = TaxonNode(parentTaxID)
 
         if taxID not in self.nodes:
-            self.nodes[taxID] = self.Node(taxID)
+            self.nodes[taxID] = TaxonNode(taxID)
 
         self.nodes[taxID].setParent(self.nodes[parentTaxID])
         self.nodes[taxID].setRank(rank)
 
-    def getNode(self, taxID: int) -> Node:
+    def getNode(self, taxID: int) -> TaxonNode:
         return self.nodes[taxID]
 
     def getTaxonomy(self, taxID: int) -> dict[str, str]:
         return self.nodes[taxID].getTaxInfo()
 
-    def toDataframe(self, rankColumns: str) -> pd.DataFrame:
+    def toDataframe(self) -> pd.DataFrame:
         dataDict = {}
         for taxID, node in self.nodes.items():
-            taxInfo = node.getTaxInfo()
-            dataDict[taxID] = {key: value for key, value in taxInfo.items() if key in rankColumns}
-            dataDict[taxID]["taxonRank"] = node.rank
-            dataDict[taxID]["higherClassification"] = ';'.join(taxInfo.values())
+            taxInfo = node.getTaxInfo(True)
+            dataDict[taxID] = {"name": node.name, "parentTaxonID": int(node.parent.id) if node.parent is not None else None}
+            dataDict[taxID] |= {key: value for key, value in taxInfo.items() if key in self.taxonRanks}
 
-        df =  pd.DataFrame.from_dict(dataDict, orient='index')
+        df = pd.DataFrame.from_dict(dataDict, orient='index')
+        df["parentTaxonID"] = df["parentTaxonID"].apply(lambda x: str(x).replace(".0", "")) # Ints still saving with 0, need this hack
         df.index.name = "taxonID"
         return df
-
+    
 def splitLine(line: str) -> list[str]:
     return [element.strip('\t') for element in line.rstrip('|\n').split('|')]
 
-def process(folderPath: Path, outputFilePath: Path) -> None:
-    tree = Tree()
-    
-    print("Adding nodes")
-    nodeData = {}
-    with open(folderPath / "nodes.dmp") as fp:
-        line = fp.readline()
+def parse(dumpFolder: Path, outputFile: Path) -> None:
+    # Build taxon tree
+    logger.info("Building taxonomy tree from nodes")
+    tree = TaxonTree()
 
-        while line:
-            taxID, parentTaxID, rank, embl, divID, iDivFlag, gcID, iGCFlag, mgcID, iMGCFlag, gHidden, sHidden, comments = splitLine(line)
+    with open(dumpFolder / DumpFiles.NODES.value) as fp:
+        line = fp.readline()
+        while "|" in line:
+            taxID, parentTaxID, rank, *_ = splitLine(line) # Ignore values beyond first 3
             tree.addNode(int(taxID), int(parentTaxID), rank)
+            
             line = fp.readline()
 
-    print("Adding names")
-    # Adding names
-    with open(folderPath / "names.dmp") as fp:
+    # Adding names to nodes
+    logger.info("Adding names to nodes")
+    with open(dumpFolder / DumpFiles.NAMES.value) as fp:
         line = fp.readline()
-        while line:
-            taxID, name, uniqueName, nameClass = splitLine(line)
-            if nameClass == 'scientific name':
-                tree.getNode(int(taxID)).setName(name)
+        while "|" in line:
+            taxID, name, _, nameClass = splitLine(line)
+            if nameClass == "scientific name":
+                node = tree.getNode(int(taxID))
+                node.setName(name)
 
             line = fp.readline()
 
-    # print("Dumping tree")
-    # with open("../generatedFiles/taxonTree", "wb") as fp:
-    #     pickle.dump(tree, fp)
-
-    print("Creating Dataframe")
-    rankColumns = ["kingdom", "phylum", "class", "order", "family", "subfamily", "genus", "subgenus", "species"]
-    df = tree.toDataframe(rankColumns)
-    
-    print(f"Writing to file {outputFilePath}")
-    outputFilePath.parent.mkdir(parents=True, exist_ok=True)
-    df.to_csv(outputFilePath)
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Get taxonomic tree for ncbi taxon ID's")
-    parser.add_argument('id', type=int)
-    args = parser.parse_args()
-
-    with open("../generatedFiles/taxonTree", "rb") as fp:
-        tree = pickle.load(fp)
-
-    for line in tree.getTaxonomy(args.id):
-        print(line)
+    logger.info("Comiling tree to dataframe and writing to file")
+    df = tree.toDataframe()
+    df.to_csv(outputFile)
