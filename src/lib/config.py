fa0a16c8210e96e308721e2255eb5bf2aa65cbcd
Christopher Mangion <christopher.mangion@hotmail.com>
2024-02-14 14:21:28 +1000

    Changed config to no longer rely on a toml file, and now just can be edited directly

--- src/lib/config.py
+++ src/lib/config.py
@@ -1,28 +1,21 @@
-import toml
 from pathlib import Path
-from dataclasses import make_dataclass, dataclass
 
-rootDir = Path(__file__).parents[2] # Parent of absolute path to src folder
-configFile = "config.toml"
+class Meta(type):
+    def __new__(cls, name, bases, attrs):
+        rootDir = Path(__file__).parents[2]
+        for k, v in attrs.items():
+            if k.startswith("__"):
+                continue
 
-configPath = rootDir / configFile
-if not configPath.exists():
-    raise Exception("No config file found!") from FileNotFoundError
+            attrs[k] = rootDir / Path(v)
 
-with open(rootDir / configFile) as fp:
-    config: dict[str, dict[str, str]] = toml.load(fp)
+        return super().__new__(cls, name, bases, attrs)
 
-files = []
-for file, path in config["files"].items():
-    path = rootDir / path if path.startswith("./") else Path(path)
-    files.append((file, Path, path))
+class Files(metaclass=Meta):
+    pass
 
-files = make_dataclass("Files", files)
-
-folders = []
-for folder, path in config["folders"].items():
-    path = path.rstrip("/")
-    path = rootDir / path if path.startswith("./") else Path(path)
-    folders.append((folder, Path, path))
-
-folders = make_dataclass("Folders", folders)
+class Folders(metaclass=Meta):
+    src: Path = "src" # Source folder for all python code
+    dataSources: Path = "dataSources" # Location of all source related files
+    mapping: Path = "mapping" # Location for map files
+    logs: Path = "logs" # Location of all logging files
