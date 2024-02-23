82c93b685d7c2abaa3fc9941fd8b3b9e8264a17c
Christopher Mangion <christopher.mangion@hotmail.com>
2024-02-23 14:44:51 +1000

    Scheduler now uses data source update method

--- src/lib/scheduling/scheduler.py
+++ src/lib/scheduling/scheduler.py
@@ -20,14 +20,6 @@ class Scheduler:
     def _getSourceTimes(self) -> dict[str, int]:
         updateTimes = {source: updater.getTimeTilUpdate() for source, updater in self.sources.items()}
         return {k: v for k, v in sorted(updateTimes.items(), key=lambda x: x[1])}
-    
-    def _update(self, source: str, extraKwargs: dict[StageFileStep, dict[str, str]] = {}) -> None:
-        database = self.manager.getDB(source)
-        database.prepareStage(StageFileStep.DWC)
-        
-        for stage in (StageFileStep.DOWNLOADED, StageFileStep.PRE_DWC, StageFileStep.DWC):
-            print(f"Creating stage: {stage.name}")
-            database.createStage(stage, overwrite=10, **extraKwargs.get(stage, {}))
 
     def run(self):
         updateTimes = self._getSourceTimes()
@@ -36,7 +28,7 @@ class Scheduler:
             for source, timeTilUpdate in updateTimes.items():
                 if timeTilUpdate == 0:
                     print(f"Updating {source}...")
-                    self._update(source)
+                    self.manager.getDB(source).update()
                     continue
 
                 sleepTime = timeTilUpdate
