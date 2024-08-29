324cc5a33c2859a86e669420d6ab8726718e3a33
Christopher Mangion <christopher.mangion@hotmail.com>
2024-08-29 11:35:52 +1000

    Fixed config objects to correctly build when imported Updated incorrectly named variable in config.toml

--- src/lib/config.py
+++ src/lib/config.py
@@ -6,24 +6,29 @@ class ConfigType(Enum):
     FILES = "files"
     FOLDERS = "folders"
         
-class Config:
-    def __init__(self, configItems: ConfigType):
+class ConfigMeta(type):
+    def __new__(cls: type, name: str, bases: tuple[str], attrs: dict):
         rootDir = Path(__file__).parents[2]
         with open(rootDir / "config.toml") as fp:
             data = toml.load(fp)
 
-        items: dict | None = data.get(configItems.value, None)
+        cfgType = attrs.get("cfg", None)
+        if cfgType is None:
+            raise AttributeError from Exception(f"No parameter `cfg` defined on object as required")
+
+        cfgValue = cfgType.value
+        items: dict | None = data.get(cfgValue, None)
         if items is None:
-            raise AttributeError from Exception(f"Invalid config item: {configItems.value}")
+            raise AttributeError from Exception(f"Invalid config item: {cfgValue}")
         
         for k, v in items.items():
             path = rootDir / Path(v) if v.startswith("./") else Path(v)
-            setattr(self, k, path)
+            attrs[k] = path
 
-class Files(Config):
-    def __init__(self):
-        super().__init__(ConfigType.FILES)
+        return super().__new__(cls, name, bases, attrs)
 
-class Folders(Config):
-    def __init__(self):
-        super().__init__(ConfigType.FOLDERS)
+class Files(metaclass=ConfigMeta):
+    cfg = ConfigType.FILES
+
+class Folders(metaclass=ConfigMeta):
+    cfg = ConfigType.FOLDERS
