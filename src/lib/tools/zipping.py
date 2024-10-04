f5a40eb21d057694d79688010e6e17d35067ec32
Christopher Mangion <christopher.mangion@hotmail.com>
2024-10-04 10:12:13 +1000

    Revert "Compressed files"

--- src/lib/tools/zipping.py
+++ src/lib/tools/zipping.py
@@ -1,8 +1,19 @@
 import zipfile
+import gzip
 import shutil
+import lzma
+import tarfile
 from pathlib import Path
 from lib.tools.logger import Logger
-        
+from enum import Enum
+import os
+
+class Extension(Enum):
+    ZIP = ".zip"
+    GZIP = ".gz"
+    XZ = ".xz"
+    TAR = ".tar"
+
 class RepeatExtractor:
     def __init__(self, outputDir: str = "", addSuffix: str = "", overwrite: bool = False):
         self.outputDir = outputDir
@@ -20,14 +31,41 @@ def extract(filePath: Path, outputDir: Path = None, addSuffix: str = "", overwri
     if outputDir is None:
         outputDir = filePath.parent
     
-    outputPath = extractsTo(filePath, outputDir, addSuffix)
-    if outputPath.exists() and not overwrite:
-        Logger.info(f"Output {outputPath.name} exists, skipping extraction stage")
-        return outputPath
+    while filePath.suffix in Extension._value2member_map_:
+        outputFile = outputDir / Path(filePath.stem)
+        if not outputFile.suffix and addSuffix: # No suffix leftover after next extract stage and provided suffix
+            outputFile = outputFile.with_suffix(addSuffix)
 
-    Logger.info(f"Extracting {filePath} to {outputPath}")
-    shutil.unpack_archive(filePath, outputPath)
-    return outputPath
+        if outputFile.exists() and not overwrite:
+            Logger.info(f"Output {outputFile.name} exists, Skipping extraction stage")
+            filePath = outputFile
+            continue
+
+        extractType = Extension(filePath.suffix)
+
+        try:
+            if extractType == Extension.ZIP:
+                with zipfile.ZipFile(filePath, 'r') as zip:
+                    zip.extractall(outputFile)
+
+            if extractType == Extension.GZIP:
+                with gzip.open(filePath, 'rb') as gz, open(outputFile, 'wb') as fp:
+                    shutil.copyfileobj(gz, fp)
+
+            if extractType == Extension.XZ:
+                with lzma.open(filePath) as xz, open(outputFile, 'wb') as fp:
+                    shutil.copyfileobj(xz, fp)
+
+            if extractType == Extension.TAR:
+                with tarfile.open(filePath, 'r') as tar:
+                    tar.extractall(outputFile, filter="data")
+        except EOFError:
+            outputFile.unlink()
+            return None
+
+        filePath = outputFile
+
+    return filePath
 
 def compress(filePath: Path, outputDir: Path = None, zipName: str = None) -> Path | None:
 
@@ -47,7 +85,7 @@ def compress(filePath: Path, outputDir: Path = None, zipName: str = None) -> Pat
     outputFile = outputDir / f"{zipName}.zip"
 
     with zipfile.ZipFile(outputFile, "w", zipfile.ZIP_DEFLATED) as zipfp:
-        if filePath.is_file():
+        if filePath.is_file:
             zipfp.write(filePath, outputFile.stem)
         else:
             compressFolder(filePath, outputFile.stem, zipfp)
@@ -55,11 +93,11 @@ def compress(filePath: Path, outputDir: Path = None, zipName: str = None) -> Pat
     return outputFile
 
 def canBeExtracted(filePath: Path) -> bool:
-    return any(suffix in (".zip", ".tar", ".gz", ".xz", ".bz2") for suffix in filePath.suffixes)
+    return filePath.suffix in Extension._value2member_map_
 
-def extractsTo(filePath: Path, outputDir: Path = None, addSuffix: str = "") -> Path:
-    outputPath = outputDir / filePath.name[:-len("".join(filePath.suffixes))]
-    if addSuffix:
-        outputPath = outputPath.with_suffix(addSuffix)
-    
-    return outputPath
+def extractsTo(filePath: Path, outputDir: Path = None) -> Path:
+    outputFile = outputDir / filePath.name if outputDir is not None else filePath
+    while outputFile.suffix in Extension._value2member_map_:
+        outputFile = outputFile.parent / outputFile.stem
+
+    return outputFile
